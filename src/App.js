5b74032995b60a6ab872fdfb8706922529b809ab
Jisoon <siriusda2@naver.com>
2019-08-15 13:15:52 +0900

    React props and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wid Web!'}
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+          <Subject 
+            title={this.state.subject.title}
+            sub={this.state.subject.sub}>
+          </Subject>
+          <TOC></TOC>
+          <Content title="HTML" sub="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
