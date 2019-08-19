d331b3fc424ef793cb9120529814f995e55f549d
金 池淳 <j_kim@tkyad.g-wise.co.jp>
2019-08-19 17:29:48 +0900

    React D-2

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -4,11 +4,21 @@ class TOC extends Component {
     render() {
       console.log('TOC render');
       var lists = [];
-      var i = 0;
       var data = this.props.data;
-      
-      while(i < data.length){
-        lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>);
+      var i = 0;
+
+      while (i < data.length){
+        lists.push(
+          <li key = {data[i].id}>
+            <a
+              href = {"/content/" + data[i].id}
+              data-id = {data[i].id}
+              onClick = {function(e){
+                e.preventDefault();
+                this.props.onChangePage(e.target.dataset.id);
+              }.bind(this)}
+            >{data[i].title}</a>
+          </li>);
         i = i + 1;
       }
 
